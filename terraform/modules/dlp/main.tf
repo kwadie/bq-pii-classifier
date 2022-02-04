ba8f9f8e65c07731c25be46cdb55de27c988b519
Karim Wadie <karim.wadie@gmail.com>
2022-02-04 17:38:26 +0100

    added rulesets example to DLP inspection template

--- terraform/modules/dlp/main.tf
+++ terraform/modules/dlp/main.tf
@@ -17,12 +17,12 @@ resource "google_data_loss_prevention_inspect_template" "inspection_template" {
 
     min_likelihood = "LIKELY"
 
-    dynamic info_types{
+    dynamic info_types {
       // filter the "standard" info types only
       for_each = [for x in var.classification_taxonomy: x if lookup(x, "info_type_category") == "standard"]
 
       content {
-        name =  lookup(info_types.value, "info_type")
+        name = lookup(info_types.value, "info_type")
       }
     }
 
@@ -37,14 +37,16 @@ resource "google_data_loss_prevention_inspect_template" "inspection_template" {
 
       dictionary {
         word_list {
-          words = ["Debit Card", "Credit Card"]
+          words = [
+            "Debit Card",
+            "Credit Card"]
         }
       }
     }
 
     #### RULE SETS
 
-
+    # Exclude a pattern of emails from the EMAIL_ADDRESS detector
     rule_set {
       info_types {
         name = "EMAIL_ADDRESS"
@@ -55,6 +57,47 @@ resource "google_data_loss_prevention_inspect_template" "inspection_template" {
             pattern = ".+@excluded-example.com"
           }
           matching_type = "MATCHING_TYPE_FULL_MATCH"
+        }
+      }
+    }
+
+    # Omit matches on PERSON_NAME detector if also matched by EMAIL_ADDRESS  detector
+    # i.e. Don't report PERSON_NAME on a column that has EMAIL_ADDRESS matches
+    # https://cloud.google.com/dlp/docs/creating-custom-infotypes-rules#omit_matches_on_person_name_detector_if_also_matched_by_email_address_detector
+
+    rule_set {
+      info_types {
+        name = "PERSON_NAME"
+      }
+      rules {
+        exclusion_rule {
+          exclude_info_types {
+            info_types {
+              name = "EMAIL_ADDRESS"
+            }
+          }
+          matching_type = "MATCHING_TYPE_FULL_MATCH"
+        }
+      }
+    }
+
+    # Increase likelihood for STREET_ADDRESS fields if the column name matches a pattern
+    # https://cloud.google.com/dlp/docs/creating-custom-infotypes-likelihood#match-column-values
+    rule_set {
+      info_types {
+        name = "STREET_ADDRESS"
+      }
+      rules {
+        hotword_rule {
+          hotword_regex {
+            pattern = "(street_name|street_address|delivery_address|house_number|city|zip)"
+          }
+          proximity {
+            window_before = 1
+          }
+          likelihood_adjustment {
+            fixed_likelihood = "VERY_LIKELY"
+          }
         }
       }
     }
